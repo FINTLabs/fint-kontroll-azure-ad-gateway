562e462020804946351c1b32693f3393a5d37e0c
Øyvind Arntzen Bratne <oyvindb@viken.no>
2023-01-10 11:44:13 +0100

    Konfigurasjon tatt ut av koden

--- src/main/java/no/fintlabs/AzureClient.java
+++ src/main/java/no/fintlabs/AzureClient.java
@@ -1,106 +1,68 @@
 package no.fintlabs;
 
-import com.azure.identity.ClientSecretCredential;
-import com.azure.identity.ClientSecretCredentialBuilder;
-import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
+import com.microsoft.graph.models.User;
+import com.microsoft.graph.requests.GraphServiceClient;
+import com.microsoft.graph.requests.UserCollectionPage;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 import okhttp3.Request;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import com.microsoft.graph.models.User;
-import com.microsoft.graph.requests.UserCollectionPage;
-import com.microsoft.graph.requests.GraphServiceClient;
 
-import javax.annotation.PostConstruct;
-import java.util.List;
-
-
-    @Component
+@Component
 @Log4j2
-/*@Configuration*/
-@ConfigurationProperties(prefix = "azurecredentials")
-    @ConstructorBinding
+//@ConfigurationProperties(prefix = "azurecredentials")
 public class AzureClient {
-    //private final WebClient webClient;
-
-    protected String CLIENT_ID;
-    protected String CLIENT_SECRET;
-    protected String TENANT_GUID;
-    protected List<String> SCOPES;
-    protected GraphServiceClient<Request> graphService;
-
-    public AzureClient() {
-        log.info("AzureClient initialized!");
-        this.CLIENT_ID = "fafcb83a-8d01-4d80-951e-a579aace4154";
-        this.CLIENT_SECRET = "kci8Q~7jENGTJXDdktW~N12mQN2LpIQrlbPYddkE";
-        this.TENANT_GUID = "3d50ddd4-00a1-4ab7-9788-decf14a8728f";
-        /*this.SCOPES = Arrays.asList("user.read");*/
-        /*this.SCOPES = Arrays.asList("/.default", "openid", "profile", "offline_access");*/
-        //this.SCOPES = Arrays.asList("testscope");
-        // TODO: Handle dependency injection
-        this.graphService = graphService();
+    protected final GraphServiceClient<Request> graphServiceClient;
+    public AzureClient(GraphServiceClient<Request> graphServiceClient) {
+        this.graphServiceClient = graphServiceClient;
         // TODO: Handle insufficient credentials
     }
-    @PostConstruct
-    public void print() {
-        log.info("Postconstruct log called!");
+
+    @Scheduled(fixedRateString = "${fint.flyt.azure-ad-gateway.resources.refresh.interval-ms}")
+    private void pulldeAllUpdatedEntities() {
     }
+
+    private void procUserPage(UserCollectionPage page) {
+        for (User user: page.getCurrentPage()) {
+            log.info("***");
+            log.info("  User: " + user.displayName);
+            log.info("  Id: " + user.id);
+            log.info("  Mail: " + user.mail);
+        }
+    }
+        
     // Fetch full user catalogue
-    @Scheduled(fixedRate=5000)
+    @Scheduled(
+            initialDelayString = "${fint.flyt.azure-ad-gateway.resources.pull.initial-delay-ms}",
+            fixedDelayString = "${fint.flyt.azure-ad-gateway.resources.pull.fixed-delay-ms}"
+    )
+    private void pullAllUpdatedEntities() {
+        log.info("--- Starting to pull resources from Azure --- ");
+        UserCollectionPage page = this.graphServiceClient.users()
+                .buildRequest()
+                //.select("displayName,id,mail")
+                /*.count(true)*/
+                //.orderBy("displayName")
+                .get();
+        // TODO: Change to while loop (while change != null;
+        //while (page != null) {
+        // TODO: Do I need some sleep time between requests?
+        if (page != null) {
+            this.procUserPage(page);
+            page = page.getNextPage()
+                    .buildRequest()
+                    .get();
+        }
+        log.info("--- finished pulling resources from Azure. ---");
+
+        // Initialize Kafka pipelines
+        //this.
+    }
+
     public void run() {
         log.info("Trigger called");
-        log.info(this.graphService.users());
-        // Populate graph with data
-
-        // https://learn.microsoft.com/en-us/graph/tutorials/java?tabs=aad&tutorial-step=9
-        // Fetch incremental user catalogue
-        UserCollectionPage page = this.graphService.users()
-                .buildRequest()
-                .select("displayName,id,mail")
-                .top(25)
-                .orderBy("displayName")
-                .get();
-        if (page != null) {
-            for (User user: page.getCurrentPage()) {
-                log.info("User: " + user.displayName);
-                log.info("Id: " + user.id);
-                log.info("Mail: " + user.mail);
-            }
-        }
-
-    }
-
-    /*@Bean
-    public ClientSecretCredential clientSecretCredential() {
-        return new ClientSecretCredentialBuilder()
-                .clientId(CLIENT_ID)
-                .clientSecret(CLIENT_SECRET)
-                .tenantId(TENANT_GUID)
-                .build();
-    }*/
-
-    /*@Bean
-    public TokenCredentialAuthProvider tokenCredentialAuthProvider() {
-        return new
-    }*/
-    @Bean
-    public GraphServiceClient<Request> graphService() {
-        ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
-                .clientId(CLIENT_ID)
-                .clientSecret(CLIENT_SECRET)
-                .tenantId(TENANT_GUID)
-                .build();
-
-        final TokenCredentialAuthProvider tokenCredentialAuthProvider = new TokenCredentialAuthProvider(SCOPES, clientSecretCredential);
-
-        return GraphServiceClient
-                .builder()
-                .authenticationProvider(tokenCredentialAuthProvider)
-                .buildClient();
+        log.info(this.graphServiceClient.users());
     }
 
     @Data
---
+++ src/main/java/no/fintlabs/Config.java
@@ -0,0 +1,53 @@
+package no.fintlabs;
+
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
+import com.microsoft.graph.requests.GraphServiceClient;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+import okhttp3.Request;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.List;
+
+
+@Log4j2
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "azurecredentials")
+public class Config {
+    private String clientid;
+    private String clientsecret;
+    private String tenantguid;
+    private String tull;
+
+    @PostConstruct
+    protected void print() {
+        log.info("--- test123 ---");
+    }
+
+    @Bean
+    public GraphServiceClient<Request> graphService() {
+        List<String> scopes = Arrays.asList("https://graph.microsoft.com/.default");
+        ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
+                .clientId(clientid)
+                .clientSecret(clientsecret)
+                .tenantId(tenantguid)
+                .build();
+
+        final TokenCredentialAuthProvider tokenCredentialAuthProvider = new TokenCredentialAuthProvider(scopes, clientSecretCredential);
+
+        return GraphServiceClient
+                .builder()
+                .authenticationProvider(tokenCredentialAuthProvider)
+                .buildClient();
+    }
+
+}
