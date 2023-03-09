18aecac12119fe176e60ac6005b77beda71d7790
Øyvind Arntzen Bratne <oyvindb@viken.no>
2023-03-09 19:03:37 +0100

    Not-working yet

--- src/main/java/no/fintlabs/AzureClient.java
+++ src/main/java/no/fintlabs/AzureClient.java
@@ -1,8 +1,13 @@
 package no.fintlabs;
 
+import com.microsoft.graph.http.BaseCollectionPage;
+import com.microsoft.graph.http.BaseRequestBuilder;
+import com.microsoft.graph.http.DeltaCollectionPage;
+import com.microsoft.graph.models.Group;
 import com.microsoft.graph.models.User;
-import com.microsoft.graph.requests.*;
-import lombok.Data;
+import com.microsoft.graph.requests.GraphServiceClient;
+import com.microsoft.graph.requests.UserCollectionPage;
+import com.microsoft.graph.requests.UserCollectionRequestBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import okhttp3.Request;
@@ -16,64 +21,82 @@ import org.springframework.stereotype.Component;
 public class AzureClient {
     protected final GraphServiceClient<Request> graphServiceClient;
     private final AzureUserProducerService azureUserProducerService;
-    /*public AzureClient(GraphServiceClient<Request> graphServiceClient) {
-        this.graphServiceClient = graphServiceClient;
-        // TODO: Handle insufficient credentials
-    }*/
-    /*private final AzureUserProducer<AzureUser> azureUserProducer;*/
+    private final AzureGroupProducerService azureGroupProducerService;
 
-    @Scheduled(fixedRateString = "${fint.flyt.azure-ad-gateway.resources.refresh.interval-ms}")
-    private void pulldeAllUpdatedEntities() {
+    private <T> void procObject(T object) {
+        if (object instanceof User) {
+            log.info("USER object detected!");
+            azureUserProducerService.publish(new AzureUser((User)object));
+        } else if (object instanceof Group) {
+            log.info("USER object detected!");
+            azureGroupProducerService.publish(new AzureGroup((Group)object));
+        } else {
+            log.info("ERROR unknown object detected!");
+        }
     }
 
-    private void procUserPage(UserCollectionPage page) {
-        for (User user : page.getCurrentPage()) {
-            log.info("***");
-            log.info("  Id: " + user.id);
-            log.info("  UPN: " + user.userPrincipalName);
-            log.info("  Mail: " + user.mail);
-            azureUserProducerService.publish(new AzureUser(user));
+    private <T1, T2 extends BaseRequestBuilder<T1>> void procPage(DeltaCollectionPage<T1, T2> page) {
+        for (T1 object : page.getCurrentPage()) {
+            this.procObject(object);
         }
+    }
+
+    private <T1, T2 extends BaseRequestBuilder<T1>> void pageThrough(DeltaCollectionPage<T1, T2> inPage) {
+        DeltaCollectionPage<T1, T2> page = inPage;
+        do {
+            this.procPage(page);
+            T2 nextPage = page.getNextPage();
+            if (nextPage == null) {
+                break;
+            } else {
+                log.info("test123");
+                page = nextPage;
+                //page = nextPage.buildRequest().get();
+            }
+        } while (page != null);
     }
 
     // Fetch full user catalogue
     @Scheduled(
-            initialDelayString = "${fint.flyt.azure-ad-gateway.resources.pull.initial-delay-ms}",
-            fixedDelayString = "${fint.flyt.azure-ad-gateway.resources.pull.fixed-delay-ms}"
+            initialDelayString = "${fint.flyt.azure-ad-gateway.users.pull.initial-delay-ms}",
+            fixedDelayString = "${fint.flyt.azure-ad-gateway.users.pull.fixed-delay-ms}"
     )
     private void pullAllEntities() {
         log.info("--- Starting to pull resources from Azure --- ");
-        UserCollectionPage page = this.graphServiceClient.users()
-                .buildRequest()
-                //.select("displayName,id,mail")
-                /*.count(true)*/
-                //.orderBy("displayName")
-                .get();
         // TODO: Change to while loop (while change != null;
-        //while (page != null) {
         // TODO: Do I need some sleep time between requests?
-        do {
-            this.procUserPage(page);
-            UserCollectionRequestBuilder nextPage = page.getNextPage();
-            if (nextPage == null) {
-                break;
-            } else {
-                page = nextPage.buildRequest().get();
-            }
-        } while (page != null);
+        this.pageThrough(
+                this.graphServiceClient.users()
+                        .delta()
+                        .buildRequest()
+                        .get()
+        );
         log.info("--- finished pulling resources from Azure. ---");
 
-        // Initialize Kafka pipelines
-        //this.
     }
+
+    @Scheduled(
+        initialDelayString = "${fint.flyt.azure-ad-gateway.groups.pull.initial-delay-ms}",
+        fixedDelayString = "${fint.flyt.azure-ad-gateway.groups.pull.delta-delay-ms}"
+    )
+    private void pullAllGroups() {
+        log.info("*** Fetching all groups from AD >>> ***");
+        this.pageThrough(
+               this.graphServiceClient.groups()
+                       .delta()
+                       .buildRequest()
+                       .get()
+        );
+        log.info("*** <<< Done fetching all groups from AD ***");
+    }
+
 
     public void run() {
         log.info("Trigger called");
         log.info(this.graphServiceClient.users());
     }
 
-    @Data
-    private static class LastUpdated {
-        private Long lastUpdated;
+    private void iterPage(BaseCollectionPage collection) {
     }
+
 }
