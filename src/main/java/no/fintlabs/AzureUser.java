ce084abb0748795a51b00aba6e7bb35ab46217e4
Ola Brustad <ola.brustad@gmail.com>
2023-04-20 10:05:24 +0200

    Testing optionaluserattribs

--- src/main/java/no/fintlabs/AzureUser.java
+++ src/main/java/no/fintlabs/AzureUser.java
@@ -1,25 +1,46 @@
 package no.fintlabs;
 
-import com.fasterxml.jackson.databind.ser.Serializers;
 import com.microsoft.graph.models.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Setter
 @Getter
 @AllArgsConstructor
+
+
 public class AzureUser extends BaseObject {
+
         private String userPrincipalName;
         private String mail;
         private String employeeId;
         private String studentId;
+        public static final String[] requiredAttributes = {
+                "id",
+                "userPrincipalName",
+                "mail",
+                "onPremisesExtensionAttributes",
 
+        };
+        @Autowired
+        private OptionalUserAttributes optionaluserattributes;
+
+        public void GetOptionalUserAttributes() {
+                List<String> allUserAttributes = optionaluserattributes.getAllUserAttributes();
+
+        }
         public AzureUser(User user) {
+                GetOptionalUserAttributes();
                 this.id = user.id;
                 this.mail = user.mail;
                 this.userPrincipalName = user.userPrincipalName;
+
+
+
                 //TODO: Parametrize attributes so they are configuratble
                 //TODO: Make sure parameters are defined
                 if (user.onPremisesExtensionAttributes != null) {
@@ -28,3 +49,7 @@ public class AzureUser extends BaseObject {
                 }
         }
 }
+
+
+
+
