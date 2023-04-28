2cdee9f78964dd34087fb48d15300b76d28b3da2
Ola Brustad <ola.brustad@gmail.com>
2023-04-28 15:48:30 +0200

    Testing optionaluserattribs from parameters

--- src/main/java/no/fintlabs/AzureUser.java
+++ src/main/java/no/fintlabs/AzureUser.java
@@ -1,50 +1,17 @@
 package no.fintlabs;
 
-import com.microsoft.graph.models.User;
+import com.microsoft.graph.models.*;
 import lombok.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Setter
 @Getter
 @RequiredArgsConstructor
-
-//@Configuration
-//@ConfigurationProperties(prefix = "fint.flyt.azure-ad-gateway")
 public class AzureUser extends BaseObject {
+        private User userObject;
 
-        private String userPrincipalName;
-        private String mail;
-        private String employeeId;
-        private String studentId;
-        private static final List<String> requiredAttributes = List.of (
-                "id",
-                "mail",
-                "onPremisesExtensionAttributes",
-                "userPrincipalName"
-        );
-        public static List<String> GetAttributes(ConfigUser configUser){
-                // TODO: 25.04.2023 Concatinate optional attribs with required attribs
-//                List<List<String>> Attribs = new ArrayList<>();
-//                Attribs.add(requiredAttributes);
-//                Attribs.add(configUser.getOptionaluserattributes());
-                return requiredAttributes;
-        };
-
-        public AzureUser(User user, ConfigUser configUser) {
+        public AzureUser(User user) {
                 this.id = user.id;
-                this.mail = user.mail;
-                this.userPrincipalName = user.userPrincipalName;
-                this.employeeId = user.getClass().getName(configUser.getEmployeeidattribute());
-                this.studentId = configUser.getStudentidattribute();
-
-                //TODO: Parametrize attributes so they are configuratble
-                //TODO: Make sure parameters are defined
-                if (user.onPremisesExtensionAttributes != null) {
-                        this.employeeId = user.onPremisesExtensionAttributes.extensionAttribute10;
-                        this.studentId = user.onPremisesExtensionAttributes.extensionAttribute9;
-                }
+                this.userObject = user;
         }
 }
 
--- src/main/java/no/fintlabs/ConfigUser.java
+++ src/main/java/no/fintlabs/ConfigUser.java
@@ -1,10 +1,13 @@
 package no.fintlabs;
 
+import com.azure.core.annotation.Get;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,10 +17,25 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ConfigUser {
+    private static List<String> requiredAttributes = Arrays.asList (
+            "id",
+            "mail",
+            "onPremisesExtensionAttributes",
+            "userPrincipalName"
+    );
 
     private List<String> optionaluserattributes = Collections.emptyList();
     private String employeeidattribute;
     private String studentidattribute;
-
+    public List<String> AllAttributes(){
+        List<String> AllAttribs = new ArrayList<>();
+        AllAttribs.add(this.getStudentidattribute());
+        AllAttribs.add(this.getEmployeeidattribute());
+        AllAttribs.addAll(requiredAttributes);
+        if(optionaluserattributes.isEmpty() == false) {
+            AllAttribs.addAll(optionaluserattributes);
+        };
+        return AllAttribs;
+    };
 
 }
