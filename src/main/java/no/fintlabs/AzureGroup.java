0028132e9a3855891eebb36ec8f07cd9441514a3
Ola Brustad <ola.brustad@gmail.com>
2023-05-02 10:14:03 +0200

    Testing group members

--- src/main/java/no/fintlabs/AzureGroup.java
+++ src/main/java/no/fintlabs/AzureGroup.java
@@ -2,15 +2,18 @@ package no.fintlabs;
 
 import com.fasterxml.jackson.databind.ser.Serializers;
 import com.microsoft.graph.models.Group;
+import com.microsoft.graph.requests.DirectoryObjectCollectionPage;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class AzureGroup extends BaseObject {
+    private DirectoryObjectCollectionPage members;
     private String name;
     public AzureGroup(Group group) {
         this.id = group.id;
         this.name = group.displayName;
+        this.members = group.members;
     }
 }
