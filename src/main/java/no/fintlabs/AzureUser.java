9f9ca5562d56ef9301abbc0277fed3173b521ad1
Ola Brustad <ola.brustad@gmail.com>
2023-05-02 13:57:24 +0200

    Testing group members. Not ready

--- src/main/java/no/fintlabs/AzureUser.java
+++ src/main/java/no/fintlabs/AzureUser.java
@@ -3,6 +3,10 @@ package no.fintlabs;
 import com.microsoft.graph.models.*;
 import lombok.*;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 @Setter
 @Getter
 @RequiredArgsConstructor
@@ -11,7 +15,27 @@ public class AzureUser extends BaseObject {
 
         public AzureUser(User user) {
                 this.id = user.id;
-                this.userObject = user;
+                this.userObject =  user;
+                Map<String, Object> results = new HashMap<>();
+                //Class<?> User = com.microsoft.graph.models.User.class;
+                Field[] fields = com.microsoft.graph.models.User.class.getDeclaredFields();
+                for (Field field : fields) {
+                        // make the field accessible to be able to read its value
+                        field.setAccessible(true);
+
+                        try {
+                                // get the value of the field from the group object
+                                Object value = field.get(user);
+
+                                // add the value to the results map if it's not null
+                                if (value != null) {
+                                        results.put(field.getName(), value);
+                                }
+                        } catch (IllegalAccessException e) {
+                                // handle the exception if the field is not accessible
+                                e.printStackTrace();
+                        }
+                }
         }
 }
 
