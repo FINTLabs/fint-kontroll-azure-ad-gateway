baf518ea4a3e29f44db58f87b72f2126555f499a
Ola Brustad <ola.brustad@gmail.com>
2023-05-11 11:27:39 +0200

    GroupObject to Kafka only contains userPrincipalName as a list of members

--- src/main/java/no/fintlabs/AzureGroup.java
+++ src/main/java/no/fintlabs/AzureGroup.java
@@ -1,8 +1,6 @@
 package no.fintlabs;
 
-import com.microsoft.graph.models.DirectoryObject;
-import com.microsoft.graph.models.Group;
-import com.microsoft.graph.models.User;
+import com.microsoft.graph.models.*;
 import com.microsoft.graph.requests.GraphServiceClient;
 import com.microsoft.graph.options.QueryOption;
 
@@ -25,16 +23,53 @@ public class AzureGroup extends BaseObject {
 
     protected String id;
     protected String displayName;
-    protected DirectoryObjectCollectionPage members;
+    protected List<String> members;
     protected List assignedLabels;
 
     public AzureGroup(Group group) {
         this.id = group.id;
         this.displayName = group.displayName;
-        this.members = group.members;
+        //this.members = group.members;
+        this.members = groupMembers(group);
         this.assignedLabels = group.assignedLabels;
     }
 
+    private List<String> groupMembers (Group group)
+    {
+        List<String> userPrincipalNames = new ArrayList<>();
+        DirectoryObjectCollectionPage groupMembers = group.members;
+        if (groupMembers != null && groupMembers.getCurrentPage() != null) {
+            List<DirectoryObject> members = groupMembers.getCurrentPage();
+
+            for (DirectoryObject member : members) {
+                if (member instanceof User) {
+                    User user = (User) member;
+                    userPrincipalNames.add(user.userPrincipalName);
+                }
+            }
+        }
+
+        return userPrincipalNames;
+
+//                        for (Field field : fields) {
+//                                // make the field accessible to be able to read its value
+//                                field.setAccessible(true);
+//                                try {
+//                                        // get the value of the field from the group object
+//                                        Object value = field.get(user);
+//
+//                                        // add the value to the results map if it's not null
+//                                        if(field.getName().endsWith(attributeName))
+//                                                if (value != null) {
+//                                                        return value.toString();
+//                                                }
+//                                } catch (IllegalAccessException e) {
+//                                        // handle the exception if the field is not accessible
+//                                        e.printStackTrace();
+//                                }
+//                        }
+//        return null;
+    }
 
 }
 
