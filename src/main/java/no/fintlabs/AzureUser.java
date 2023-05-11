e15f0e5529e9b9f5cf2f686da31e6d1b654d4327
Ola Brustad <ola.brustad@gmail.com>
2023-05-11 09:57:49 +0200

    Student and EmployeeId values returned to user object to Kafka, simplified to not for loop

--- src/main/java/no/fintlabs/AzureUser.java
+++ src/main/java/no/fintlabs/AzureUser.java
@@ -32,67 +32,73 @@ public class AzureUser {
                 this.givenname = user.givenName;
                 this.userPrincipalName = user.userPrincipalName;
                 this.onPremisesUserPrincipalName = user.onPremisesUserPrincipalName;
-                try {
-                        this.employeeId = (String) getAttributeValue(user, configUser.getEmployeeidattribute());
-                } catch (Exception e) {
-                        log.info(e);
-
-                }
-                try {
-
-                        this.studentId = (String) getAttributeValue(user, configUser.getStudentidattribute());
-                } catch (Exception e) {
-                        log.info(e);
-                }
-
+                this.employeeId = getAttributeValue(user, configUser.getEmployeeidattribute());
+                this.studentId = getAttributeValue(user, configUser.getStudentidattribute());
         }
 
 
-        public static Object getAttributeValue(User user, String attributeName) throws NoSuchFieldException, IllegalAccessException {
+        public static String getAttributeValue(User user, String attributeName) {
                 // Split the attribute name by dot to get the nested field names
                 String[] attributeParts = attributeName.split("\\.");
 
                 if (attributeParts[0].equals("onPremisesExtensionAttributes")) {
                         OnPremisesExtensionAttributes attributeValues = user.onPremisesExtensionAttributes;
-                        Field[] fields = OnPremisesExtensionAttributes.class.getDeclaredFields();
-                        for (Field field : fields) {
-                                // make the field accessible to be able to read its value
-                                field.setAccessible(true);
-                                try {
-                                        // get the value of the field from the group object
-                                        Object value = field.get(attributeValues);
-
-                                        // add the value to the results map if it's not null
-                                        if(field.getName().endsWith(attributeParts[1]))
-                                                if (value != null)
-                                                        return value.toString();
-
-                                } catch (IllegalAccessException e) {
-                                        // handle the exception if the field is not accessible
-                                        e.printStackTrace();
+                        try {
+                                Field _field = OnPremisesExtensionAttributes.class.getDeclaredField(attributeParts[1]);
+                                _field.setAccessible(true);
+                                Object value = _field.get(attributeValues);
+                                if(_field.getName().endsWith(attributeParts[1]) && value != null)
+                                        return value.toString();
                         }
-                        }
+                        catch (Exception e)
+                        {}
+//                        for (Field field : fields) {
+//                                // make the field accessible to be able to read its value
+//                                field.setAccessible(true);
+//                                try {
+//                                        // get the value of the field from the group object
+//                                        Object value = field.get(attributeValues);
+//
+//                                        // add the value to the results map if it's not null
+//                                        if(field.getName().endsWith(attributeParts[1]))
+//                                                if (value != null)
+//                                                        return value.toString();
+//
+//                                } catch (IllegalAccessException e) {
+//                                        // handle the exception if the field is not accessible
+//                                        e.printStackTrace();
+//                        }
+//                        }
                 }
                 else
                 {
-                        Field[] fields = User.class.getDeclaredFields();
-                        for (Field field : fields) {
-                                // make the field accessible to be able to read its value
-                                field.setAccessible(true);
-                                try {
-                                        // get the value of the field from the group object
-                                        Object value = field.get(user);
-
-                                        // add the value to the results map if it's not null
-                                        if(field.getName().endsWith(attributeName))
-                                        if (value != null) {
-                                                return value.toString();
-                                        }
-                                } catch (IllegalAccessException e) {
-                                        // handle the exception if the field is not accessible
-                                        e.printStackTrace();
-                                }
+                        try {
+                                Field _field = OnPremisesExtensionAttributes.class.getDeclaredField(attributeName);
+                                _field.setAccessible(true);
+                                Object value = _field.get(user);
+                                if(_field.getName().endsWith(attributeName) && value != null)
+                                        return value.toString();
                         }
+                        catch (Exception e)
+                        {}
+//                        Field[] fields = User.class.getDeclaredFields();
+//                        for (Field field : fields) {
+//                                // make the field accessible to be able to read its value
+//                                field.setAccessible(true);
+//                                try {
+//                                        // get the value of the field from the group object
+//                                        Object value = field.get(user);
+//
+//                                        // add the value to the results map if it's not null
+//                                        if(field.getName().endsWith(attributeName))
+//                                                if (value != null) {
+//                                                        return value.toString();
+//                                                }
+//                                } catch (IllegalAccessException e) {
+//                                        // handle the exception if the field is not accessible
+//                                        e.printStackTrace();
+//                                }
+//                        }
                 }
 
                 // Return the attribute value
