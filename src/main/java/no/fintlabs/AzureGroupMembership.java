9bb494f3056d862f8cf1bfdc85a0998c179671a9
Øyvind Arntzen Bratne <oyvindb@viken.no>
2023-06-06 13:14:51 +0200

    Working version

--- src/main/java/no/fintlabs/AzureGroupMembership.java
+++ src/main/java/no/fintlabs/AzureGroupMembership.java
@@ -5,9 +5,15 @@ import com.microsoft.graph.models.Group;
 import com.microsoft.graph.models.GroupMembers;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.codec.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.security.MessageDigest;
 
 @Getter
 @Setter
+@Log4j2
 public class AzureGroupMembership extends BaseObject {
     private String user_id;
     private String group_id;
@@ -15,6 +21,8 @@ public class AzureGroupMembership extends BaseObject {
     public AzureGroupMembership(String group_id, DirectoryObject directoryObject) {
         this.group_id = group_id;
         this.user_id = directoryObject.id;
+        //this.id = DigestUtils.sha3_512Hex(this.group_id + this.user_id);
+        this.id = this.group_id + this.user_id;
     }
     /*public AzureGroupMembership(T membership) {
         this.id = membership.groupId;
