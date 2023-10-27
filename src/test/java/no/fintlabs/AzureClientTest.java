7ff71d018e3020e266cfdc4a268a83d3b1860836
Øyvind Bratne <89254037+bratne@users.noreply.github.com>
2023-10-27 15:14:56 +0200

    Implemented a few tests. Still unfinished

--- src/test/java/no/fintlabs/AzureClientTest.java
+++ src/test/java/no/fintlabs/AzureClientTest.java
@@ -1,22 +1,81 @@
 package no.fintlabs;
 
+import com.microsoft.graph.models.Group;
+import com.microsoft.graph.requests.GraphServiceClient;
+import com.microsoft.graph.requests.GroupCollectionPage;
+import com.microsoft.graph.requests.GroupCollectionRequest;
+import com.microsoft.graph.requests.GroupCollectionRequestBuilder;
+import no.fintlabs.azure.AzureGroup;
 import no.fintlabs.kafka.ResourceGroup;
+import okhttp3.Request;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 class AzureClientTest {
+    @Mock
+    private GraphServiceClient<Request> graphServiceClient;
+    @Mock
+    private GroupCollectionPage groupCollectionPage;
+    @Mock
+    private GroupCollectionRequest groupCollectionRequest;
+    @Mock
+    private GroupCollectionRequestBuilder groupCollectionRequestBuilder;
+    @Mock ConfigGroup configGroup;
+    @Mock Group group;
+
+    @InjectMocks
+    private AzureClient azureClient;
 
     @Test
+    void doesGroupExist_found() {
+        String resourceGroupID = "123";
+        //when(azureClient.doesGroupExist(resourceGroupID)).thenReturn(false);
+
+
+        when(graphServiceClient.groups()).thenReturn(groupCollectionRequestBuilder);
+        when(graphServiceClient.groups().buildRequest()).thenReturn(groupCollectionRequest);
+        when(graphServiceClient.groups().buildRequest().select(anyString())).thenReturn(groupCollectionRequest);
+        when(graphServiceClient.groups().buildRequest().get()).thenReturn(groupCollectionPage);
+
+
+        List<Group> groupList = new ArrayList<Group>();
+        Group group = new Group();
+        group.displayName = "testgroup1";
+        groupList.add(group);
+        group.displayName = "testgroup2";
+        groupList.add(group);
+        when(groupCollectionPage.getCurrentPage()).thenReturn(groupList);
+
+        boolean checkvar = azureClient.doesGroupExist(resourceGroupID);
+        assertTrue(checkvar);
+    }
+    @Test
+    void doesGroupExist_notfound() {
+
+    }
+    @Test
     void doesGroupExist() {
-        String resourceGroupID = "testresourcegroup";
-        ResourceGroup resourceGroup = new ResourceGroup(resourceGroupID, "testresource", "testDisplayname", "testidp", "testresourcename", "testresourcetype", "testresourcelimit");
+        String resourceGroupID = "12";
+        //ResourceGroup resourceGroup = new ResourceGroup(resourceGroupID, "testresource", "testDisplayname", "testidp", "testresourcename", "testresourcetype", "testresourcelimit");
+        //doesGroupExist(resourceGroupID);
+        //azureClient.doesGroupExist("");
+        //String resourceGroupID = "testresourcegroup";
+        //ResourceGroup resourceGroup = new ResourceGroup(resourceGroupID, "testresource", "testDisplayname", "testidp", "testresourcename", "testresourcetype", "testresourcelimit");
 
-        when(azureClient.doesGroupExist(resourceGroupID)).thenReturn(false);
+        //when(azureClient.doesGroupExist(resourceGroupID)).thenReturn(false);
 
-        resourceGroupConsumerService.processEntity(resourceGroup, null);
-        verify(azureClient, times(1));
+        //resourceGroupConsumerService.processEntity(resourceGroup, null);
+        //verify(azureClient, times(1));
 
         // asserts (hvis returnvalue)
         // verify (mockito)
