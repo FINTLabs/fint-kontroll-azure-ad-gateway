3a5b1dbf63dab4cf9c1458eb7379b8a81646ba0f
Øyvind Bratne <89254037+bratne@users.noreply.github.com>
2023-10-27 12:46:26 +0200

    doesGroupExist now searches all pages from MS Graph. This solves [FKS-197]. Unfinished with testing

--- src/test/java/no/fintlabs/AzureClientTest.java
+++ src/test/java/no/fintlabs/AzureClientTest.java
@@ -1,7 +1,38 @@
 package no.fintlabs;
 
+import no.fintlabs.kafka.ResourceGroup;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class AzureClientTest {
 
+    @Test
+    void doesGroupExist() {
+        String resourceGroupID = "testresourcegroup";
+        ResourceGroup resourceGroup = new ResourceGroup(resourceGroupID, "testresource", "testDisplayname", "testidp", "testresourcename", "testresourcetype", "testresourcelimit");
+
+        when(azureClient.doesGroupExist(resourceGroupID)).thenReturn(false);
+
+        resourceGroupConsumerService.processEntity(resourceGroup, null);
+        verify(azureClient, times(1));
+
+        // asserts (hvis returnvalue)
+        // verify (mockito)
+        // Kan også verifisere input til kall. Sjekk parameterverdien er innafor <a,b> osv
+
+    }
+
+    @Test
+    void addGroupToAzure() {
+    }
+
+    @Test
+    void deleteGroup() {
+    }
+
+    @Test
+    void updateGroup() {
+    }
 }
