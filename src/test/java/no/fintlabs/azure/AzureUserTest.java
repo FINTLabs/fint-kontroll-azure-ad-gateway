660a16eed0e90b67b047bcb4bd5071a80a0652ea
Øyvind Bratne <89254037+bratne@users.noreply.github.com>
2023-11-16 13:25:25 +0100

    Some unfinished testing

---
+++ src/test/java/no/fintlabs/azure/AzureUserTest.java
@@ -0,0 +1,39 @@
+package no.fintlabs.azure;
+
+import no.fintlabs.ConfigUser;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import com.microsoft.graph.models.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AzureUserTest {
+    @Mock
+    private ConfigUser configUser;
+
+    @Test
+    public void makeSureUserConversionIsAsExpected() {
+
+        /*when(configUser.getEmployeeidattribute()).thenReturn("country");
+        when(configUser.getStudentidattribute()).thenReturn("preferredLanguage");
+
+        User user = new User();
+        user.id = "123";
+        user.mail = "testuser@mail.com";
+        user.userPrincipalName = "testuser@mail.com";
+
+        AzureUser convertedUser = new AzureUser(user, configUser);
+
+        assert(convertedUser.getId() == user.id );
+        assert(convertedUser.getIdpUserObjectId() == user.id);
+        assert(convertedUser.getMail() == user.mail );
+        assert(convertedUser.getUserPrincipalName() == user.userPrincipalName);
+        assert(convertedUser.getEmployeeId() == "SomeValue");
+        assert(convertedUser.getStudentId() == "SomeValue3");*/
+
+    }
+}
