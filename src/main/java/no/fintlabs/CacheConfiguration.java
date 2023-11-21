4389d9861e59c45ef7ec7b844220404c7aa08aa3
Ola Brustad <ola.brustad@gmail.com>
2023-11-21 13:47:10 +0100

    Cache

--- src/main/java/no/fintlabs/CacheConfiguration.java
+++ src/main/java/no/fintlabs/CacheConfiguration.java
@@ -1,34 +1,34 @@
 package no.fintlabs;
+import lombok.AllArgsConstructor;
 import no.fintlabs.azure.AzureGroup;
 import no.fintlabs.cache.FintCache;
 import no.fintlabs.cache.FintCacheManager;
 import no.fintlabs.kafka.ResourceGroup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Locale;
+@AllArgsConstructor
 @Configuration
 public class CacheConfiguration {
 
-
     private final FintCacheManager fintCacheManager;
 
-    public ResourceEntityCacheConfiguration(FintCacheManager fintCacheManager) {
-        this.fintCacheManager = fintCacheManager;
-
-        @Bean
-        FintCache<String, ResourceGroup> resourceGroupCache () {
-            return createResourceCache(ResourceGroup.class);
-        }
-
-        @Bean
-        FintCache<String, AzureGroup> azureGroupCache () {
-            return createResourceCache(AzureGroup.class);
-        }
+    @Bean
+    FintCache<String, ResourceGroup> resourceGroupCache() {
+        return createResourceCache(ResourceGroup.class);
     }
-        private <V> FintCache<String, V> createResourceCache(Class<V> resourceClass) {
-            return fintCacheManager.createCache(
-                    resourceClass.getName().toLowerCase(Locale.ROOT),
-                    String.class,
-                    resourceClass
-            );
-        }
+
+    @Bean
+    FintCache<String, AzureGroup> azureGroupCache() {
+        return createResourceCache(AzureGroup.class);
+    }
+
+    private <V> FintCache<String, V> createResourceCache(Class<V> resourceClass) {
+        return fintCacheManager.createCache(
+                resourceClass.getName().toLowerCase(Locale.ROOT),
+                String.class,
+                resourceClass
+        );
+    }
 }
