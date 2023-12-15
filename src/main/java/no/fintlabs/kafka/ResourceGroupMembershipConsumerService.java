8e8617cd24402db9aec8c911d471da7fa6043a07
Øyvind Bratne <89254037+bratne@users.noreply.github.com>
2023-12-15 15:39:54 +0100

    Added cache-usage. Solves FKS-369

--- src/main/java/no/fintlabs/kafka/ResourceGroupMembershipConsumerService.java
+++ src/main/java/no/fintlabs/kafka/ResourceGroupMembershipConsumerService.java
@@ -1,24 +1,22 @@
 package no.fintlabs.kafka;
 
 
-import com.microsoft.graph.http.GraphServiceException;
-import com.microsoft.graph.models.DirectoryObject;
-import com.microsoft.graph.requests.GraphServiceClient;
-import lombok.AllArgsConstructor;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.AzureClient;
 import no.fintlabs.Config;
+import no.fintlabs.cache.FintCache;
 import no.fintlabs.kafka.entity.EntityConsumerFactoryService;
 import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
-import okhttp3.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Sinks;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
-import jakarta.annotation.PostConstruct;
-import java.util.Objects;
+import java.util.UUID;
 
 @Service
-@AllArgsConstructor
 @Slf4j
 
 public class ResourceGroupMembershipConsumerService {
@@ -26,6 +24,23 @@ public class ResourceGroupMembershipConsumerService {
     private final AzureClient azureClient;
     private final EntityConsumerFactoryService entityConsumerFactoryService;
     private final Config config;
+    private final FintCache<String, ResourceGroupMembership> resourceGroupMembershipCache;
+    private final Sinks.Many<Tuple2<String, ResourceGroupMembership>> resourceGroupMembershipSink;
+
+    public ResourceGroupMembershipConsumerService(
+            AzureClient azureClient,
+            EntityConsumerFactoryService entityConsumerFactoryService,
+            Config config,
+            FintCache<String, ResourceGroupMembership> resourceGroupMembershipCache) {
+        this.azureClient = azureClient;
+        this.entityConsumerFactoryService = entityConsumerFactoryService;
+        this.config = config;
+        this.resourceGroupMembershipCache = resourceGroupMembershipCache;
+        this.resourceGroupMembershipSink = Sinks.many().unicast().onBackpressureBuffer();
+        this.resourceGroupMembershipSink.asFlux().subscribe(
+                keyAndResourceGroupMembership -> updateAzureWithMembership(keyAndResourceGroupMembership.getT1(), keyAndResourceGroupMembership.getT2())
+        );
+    }
 
     @PostConstruct
     public void init() {
@@ -38,20 +53,41 @@ public class ResourceGroupMembershipConsumerService {
                         .build()
         );
     }
-    public void processEntity(ResourceGroupMembership resourceGroupMembership, String resourceGroupMembershipKey) {
 
-        if (resourceGroupMembershipKey == null) {
-            log.error("Error when processing entity. Kafka key (objekt id in kafka) is not set. Unsupported!");
-            return;
-        }
-        // Already existing membership
+    private synchronized void updateAzureWithMembership(String kafkakKey, ResourceGroupMembership resourceGroupMembership) {
+        String randomUUID = UUID.randomUUID().toString();
+        log.debug("Starting updateAzureWithMembership function {}.", randomUUID);
+
         if (resourceGroupMembership == null) {
-            azureClient.deleteGroupMembership(resourceGroupMembership, resourceGroupMembershipKey);
+            azureClient.deleteGroupMembership(resourceGroupMembership, kafkakKey);
         }
         else
         {
-            azureClient.addGroupMembership(resourceGroupMembership, resourceGroupMembershipKey);
+            azureClient.addGroupMembership(resourceGroupMembership, kafkakKey);
         }
+        log.debug("Stopping updateAzureWithMembership function {}.", randomUUID);
+    }
+
+    public void processEntity(ResourceGroupMembership resourceGroupMembership, String kafkaKey) {
+
+        if (kafkaKey == null) {
+            log.error("Error when processing entity. Kafka key (objekt id in kafka) is not set. Unsupported!");
+            return;
+        }
+        synchronized (resourceGroupMembershipCache) {
+            // Check resourceGroupCache if object is known from before
+            if (resourceGroupMembershipCache.containsKey(kafkaKey)) {
+                ResourceGroupMembership fromCache = resourceGroupMembershipCache.get(kafkaKey);
+                if (resourceGroupMembershipCache.equals(fromCache)){
+                    // New kafka message, but unchanged resourceGroupMembership from last time
+                    log.debug("Skipping processing of group membership, as it is unchanged from before: userID: {} groupID {}", resourceGroupMembership.getAzureUserRef(), resourceGroupMembership.getAzureGroupRef() );
+                    return;
+                }
+            }
+            resourceGroupMembershipCache.put(kafkaKey, resourceGroupMembership);
+            resourceGroupMembershipSink.tryEmitNext(Tuples.of(kafkaKey, resourceGroupMembership));
+        }
+
     }
 }
 
