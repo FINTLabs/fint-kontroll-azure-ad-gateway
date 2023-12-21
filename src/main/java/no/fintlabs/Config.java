ae535b94ce29c90b1bc82498a78a8f4cc0f0c522
Øyvind Bratne <89254037+bratne@users.noreply.github.com>
2023-12-21 18:03:04 +0100

    New version of CD to deploy to AFK

--- src/main/java/no/fintlabs/Config.java
+++ src/main/java/no/fintlabs/Config.java
@@ -2,12 +2,24 @@ package no.fintlabs;
 
 import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
+import com.microsoft.graph.authentication.IAuthenticationProvider;
 import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
+import com.microsoft.graph.http.CoreHttpProvider;
+import com.microsoft.graph.http.IHttpProvider;
+import com.microsoft.graph.logger.DefaultLogger;
+import com.microsoft.graph.logger.ILogger;
 import com.microsoft.graph.requests.GraphServiceClient;
+import com.microsoft.graph.serializer.DefaultSerializer;
+import com.microsoft.graph.serializer.ISerializer;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
+import okhttp3.Authenticator;
 import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.Route;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -15,8 +27,10 @@ import org.springframework.context.annotation.Configuration;
 //import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PostConstruct;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 @Log4j2
@@ -32,6 +46,7 @@ public class Config {
 
     private String entobjectid;
 
+    private IHttpProvider<Request> httpProvider;
 
     @PostConstruct
     protected void print() {
@@ -62,9 +77,33 @@ public class Config {
 
         final TokenCredentialAuthProvider tokenCredentialAuthProvider = new TokenCredentialAuthProvider(scopes, clientSecretCredential);
 
-        return GraphServiceClient
+
+        // Create a custom HTTP provider with adjusted timeout
+        okhttp3.OkHttpClient okHttpClient = new okhttp3.OkHttpClient.Builder()
+                .readTimeout(100, TimeUnit.MILLISECONDS)  // Set read timeout to 30 seconds
+                .authenticator(new Authenticator() {
+                    @Nullable
+                    @Override
+                    public Request authenticate(@Nullable Route route, @NotNull Response response) throws IOException {
+                        return null;
+                    }
+                })
+                .build();
+
+        ILogger httpLogger = new DefaultLogger();
+        ISerializer httpSerializer = new DefaultSerializer(httpLogger);
+
+        this.httpProvider =  new CoreHttpProvider(httpSerializer, httpLogger, okHttpClient);
+
+        GraphServiceClient graphServiceClient = GraphServiceClient
                 .builder()
                 .authenticationProvider(tokenCredentialAuthProvider)
+                .httpProvider(this.httpProvider)
                 .buildClient();
+
+        graphServiceClient.getHttpProvider().
+
+        return graphServiceClient;
+
     }
 }
