440ebfc5b15839d4d30a49e397ec1b94c1da31fa
Ola Brustad <ola.brustad@gmail.com>
2024-04-30 15:36:44 +0200

    Added Orchestrator - testing

--- src/main/java/no/fintlabs/Orchestrator.java
+++ src/main/java/no/fintlabs/Orchestrator.java
@@ -1,10 +1,26 @@
 package no.fintlabs;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import no.fintlabs.kafka.ResourceGroupMembershipDownloader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 
 @Component
+@AllArgsConstructor
+
 public class Orchestrator {
 
+    private final ResourceGroupMembershipDownloader resourceGroupMembershipDownloader;
+
+    @Scheduled(
+            initialDelayString = "${fint.kontroll.azure-ad-gateway.user-scheduler.pull.initial-delay-ms}",
+            fixedDelayString = "${fint.kontroll.azure-ad-gateway.user-scheduler.pull.fixed-delay-ms}"
+    )
+    private void pullAllMemberships() {
+        resourceGroupMembershipDownloader.callApi();
+    }
 }
 
---
+++ src/main/java/no/fintlabs/kafka/ResourceGroupMembershipDownloader.java
@@ -0,0 +1,127 @@
+package no.fintlabs.kafka;
+
+import jakarta.annotation.PostConstruct;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.AzureClient;
+import no.fintlabs.Config;
+import no.fintlabs.cache.FintCache;
+import no.fintlabs.kafka.entity.EntityConsumerFactoryService;
+import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@Slf4j
+public class ResourceGroupMembershipDownloader {
+    @Autowired
+    private final AzureClient azureClient;
+    private final EntityConsumerFactoryService entityConsumerFactoryService;
+    private final Config config;
+
+    private RestTemplate restTemplate;
+    private final FintCache<String, Optional> resourceGroupMembershipCache;
+    private final Sinks.Many<Tuple2<String, Optional<ResourceGroupMembership>>> resourceGroupMembershipSink;
+
+
+    public ResourceGroupMembershipDownloader(
+            AzureClient azureClient,
+            EntityConsumerFactoryService entityConsumerFactoryService,
+            Config config,
+            FintCache<String, Optional> resourceGroupMembershipCache) {
+        this.restTemplate = new RestTemplate();
+        this.azureClient = azureClient;
+        this.entityConsumerFactoryService = entityConsumerFactoryService;
+        this.config = config;
+        this.resourceGroupMembershipCache = resourceGroupMembershipCache;
+        this.resourceGroupMembershipSink = Sinks.many().unicast().onBackpressureBuffer();
+//        this.resourceGroupMembershipSink.asFlux().subscribe(
+//                keyAndResourceGroupMembership -> updateAzureWithMembership(keyAndResourceGroupMembership.getT1(), keyAndResourceGroupMembership.getT2())
+//        );
+    }
+
+    public HttpStatusCode callApi() {
+        String apiUrl = "http://localhost:3030/api/assignments/republish";
+        HttpEntity<String> requestEntity = new HttpEntity<>(null, null);
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(apiUrl, requestEntity, String.class);
+        HttpStatusCode StatusCode = responseEntity.getStatusCode();
+        return StatusCode;
+    }
+
+
+//    public void GetFullMembership() {
+//        //TODO: Fix sensible throw when parsing wrong data. Non-json-formatted data fails [FKS-214]
+//        entityConsumerFactoryService.createFactory(ResourceGroupMembership.class, consumerRecord -> processEntity(consumerRecord.value(), consumerRecord.key())
+//        ).createContainer(
+//                EntityTopicNameParameters
+//                        .builder()
+//                        .resource("resource-group-membership-full")
+//                        .build()
+//        );
+//    }
+//
+//    private synchronized void updateAzureWithMembership(String kafkakKey, Optional<ResourceGroupMembership> resourceGroupMembership) {
+//        String randomUUID = UUID.randomUUID().toString();
+//        log.debug("Starting updateAzureWithMembership function {}.", randomUUID);
+//
+//        if (resourceGroupMembership.isEmpty()) {
+//            azureClient.deleteGroupMembership(null, kafkakKey);
+//        }
+//        else
+//        {
+//            azureClient.addGroupMembership(resourceGroupMembership.get(), kafkakKey);
+//        }
+//        log.debug("Stopping updateAzureWithMembership function {}.", randomUUID);
+//    }
+//
+//    public void processEntity(ResourceGroupMembership resourceGroupMembership, String kafkaKey) {
+//
+//        if (kafkaKey == null || (resourceGroupMembership != null && (resourceGroupMembership.getAzureGroupRef() == null || resourceGroupMembership.getAzureUserRef() == null))) {
+//            log.error("Error when processing entity. Kafka key or values is null. Unsupported!. ResourceGroupMembership object: {}",
+//                    (resourceGroupMembership != null ? resourceGroupMembership : "null"));
+//            return;
+//        }
+//
+//        synchronized (resourceGroupMembershipCache) {
+//            // Check resourceGroupCache if object is known from before
+//            log.debug("Processing entity with key: {}", kafkaKey);
+//
+//            if (resourceGroupMembershipCache.containsKey(kafkaKey)) {
+//                log.debug("Found key in cache: {}", kafkaKey);
+//
+//                Optional<ResourceGroupMembership> fromCache = resourceGroupMembershipCache.get(kafkaKey);
+//
+//                log.debug("From cache: {}", fromCache);
+//
+//                if (fromCache.isEmpty() && resourceGroupMembership == null) {
+//                    // resourceGroupMembership is a delete message already in cache
+//                    log.debug("Skipping processing of already cached delete group membership message: {}",kafkaKey);
+//                    return;
+//                }
+//
+//                if (resourceGroupMembership != null && fromCache.isPresent() && resourceGroupMembership.equals(fromCache.get())){
+//                    // New kafka message, but unchanged resourceGroupMembership from last time
+//                    log.debug("Skipping processing of group membership, as it is unchanged from before: userID: {} groupID {}", resourceGroupMembership.getAzureUserRef(), resourceGroupMembership.getAzureGroupRef() );
+//                    return;
+//                }
+//            }
+//            resourceGroupMembershipCache.put(kafkaKey, Optional.ofNullable(resourceGroupMembership));
+//            resourceGroupMembershipSink.tryEmitNext(Tuples.of(kafkaKey, Optional.ofNullable(resourceGroupMembership)));
+//        }
+
+//    }
+}
+
+
