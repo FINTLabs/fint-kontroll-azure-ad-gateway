5427983522f297aba4f0accfb9090ab3696aca5a
Ola Brustad <ola.brustad@gmail.com>
2024-10-23 12:36:11 +0200

    [FKS-983] api/user/{objectId}, returns user and group memberships

--- src/main/java/no/fintlabs/rest/LookupService.java
+++ src/main/java/no/fintlabs/rest/LookupService.java
@@ -1,5 +1,6 @@
 package no.fintlabs.rest;
 
+import com.microsoft.graph.models.Group;
 import com.microsoft.graph.models.User;
 import com.microsoft.graph.serviceclient.GraphServiceClient;
 import com.microsoft.graph.users.item.getmembergroups.GetMemberGroupsPostRequestBody;
@@ -9,7 +10,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import no.fintlabs.ConfigGroup;
 import no.fintlabs.ConfigUser;
+import no.fintlabs.azure.AzureGroup;
+import no.fintlabs.azure.AzureUser;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Log4j2
@@ -20,26 +26,32 @@ public class LookupService {
     protected final GraphServiceClient graphServiceClient;
 
     public UserWithGroupsDto getAzureUserWithGroups(String objectId) {
-        String[] selectionCriteria = new String[]{String.join(",", configUser.AllAttributes())};
-        User user = null;
-        GetMemberGroupsPostResponse userGroupMember = null;
-
         try {
-            // Fetch the user information
-            user = graphServiceClient.users().byUserId(objectId).get(requestConfiguration -> {
-                requestConfiguration.queryParameters.select = selectionCriteria;
-            });
+            AzureUser azureUser = new AzureUser(graphServiceClient.users().byUserId(objectId).get(), configUser);
 
-            // Fetch the group memberships
-            userGroupMember = graphServiceClient.users().byUserId(user.getId()).getMemberGroups().post(new GetMemberGroupsPostRequestBody());
+            GetMemberGroupsPostRequestBody requestBody = new GetMemberGroupsPostRequestBody();
+            requestBody.setSecurityEnabledOnly(true);
+            List<String> groupIds = graphServiceClient.users().byUserId(objectId).getMemberGroups().post(requestBody).getValue();
 
-            log.info("*** <<< Found user {} from getAzureUser in API >>> ***", user.getUserPrincipalName());
+            String[] selectionCriteriaGroup = new String[]{String.format("id,displayName,%s", configGroup.getFintkontrollidattribute())};
+            List<AzureGroup> azureGroups = new ArrayList<>();
+            for (String groupId : groupIds) {
+                Group group = graphServiceClient
+                        .groups()
+                        .byGroupId(groupId)
+                        .get(requestConfiguration -> {
+                                    requestConfiguration.queryParameters.select = selectionCriteriaGroup; });
 
-            // Return both user and groups in the DTO
-            return new UserWithGroupsDto(user, userGroupMember.getValue());
+                AzureGroup azureGroup = new AzureGroup(group, configGroup);
+                azureGroups.add(azureGroup);
+            }
+            log.info("*** <<< Found user {} from getAzureUser in API >>> ***", azureUser.getUserPrincipalName());
+
+            return UserWithGroupsDto.UserWithGroups(azureUser, azureGroups);
+
         } catch (ApiException ex) {
             log.error("Failed to fetch user or groups: {}", ex.getMessage());
-            return null;  // Handle error cases appropriately
+            return new UserWithGroupsDto();
         }
     }
 }
