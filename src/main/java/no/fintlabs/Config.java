fc7fa6928225503ec21030a33b4bb709fb3a9bfd
Ola Brustad <ola.brustad@gmail.com>
2024-12-16 12:21:52 +0100

    [FKS-798] work in progress. Kafka config

--- src/main/java/no/fintlabs/Config.java
+++ src/main/java/no/fintlabs/Config.java
@@ -1,16 +1,22 @@
 package no.fintlabs;
 
+import com.azure.core.http.policy.RetryPolicy;
+import com.azure.core.util.HttpClientOptions;
 import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
+import com.microsoft.graph.core.requests.GraphClientFactory;
 import com.microsoft.graph.serviceclient.GraphServiceClient;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 
+import com.azure.core.http.HttpClient;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 @Log4j2
 @Getter
@@ -23,10 +29,6 @@ public class Config {
     private String clientsecret;
     private String tenantguid;
     private String entobjectid;
-
-    protected void print() {
-        log.debug("Starting PostConstruct");
-    }
 
     @Bean
     @ConfigurationProperties(prefix = "fint.kontroll.azure-ad-gateway.user")
@@ -42,9 +44,18 @@ public class Config {
 
     @Bean
     public GraphServiceClient graphServiceClient(){
+        log.debug("Starting PostConstruct of GraphServiceClient");
         String[] scopes = new String[] {"https://graph.microsoft.com/.default"};
 
+        HttpClientOptions options = new HttpClientOptions();
+        options.setConnectTimeout(Duration.ofMinutes(5));
+        options.setReadTimeout(Duration.ofMinutes(5));
+        options.setWriteTimeout(Duration.ofMinutes(5));
+
+        HttpClient httpClient = HttpClient.createDefault(options);
+
         ClientSecretCredential credential = new ClientSecretCredentialBuilder()
+                .httpClient(httpClient)
                 .clientId(clientid)
                 .tenantId(tenantguid)
                 .clientSecret(clientsecret)
