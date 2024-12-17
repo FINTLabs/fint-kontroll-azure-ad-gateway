7ad858f93c680fe9304b6d66e9895f6fa72403d2
Ola Brustad <ola.brustad@gmail.com>
2024-12-17 14:15:17 +0100

    [FKS-947] Removed old stuff from Config file

--- src/main/java/no/fintlabs/Config.java
+++ src/main/java/no/fintlabs/Config.java
@@ -1,29 +1,17 @@
 package no.fintlabs;
 
-import com.azure.core.credential.TokenCredential;
-import com.azure.core.util.HttpClientOptions;
 import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
 import com.microsoft.graph.serviceclient.GraphServiceClient;
-import com.microsoft.kiota.RequestInformation;
-import com.microsoft.kiota.authentication.AuthenticationProvider;
 import com.microsoft.kiota.authentication.AzureIdentityAuthenticationProvider;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
-
-import com.azure.core.http.HttpClient;
 import okhttp3.OkHttpClient;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.Duration;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Log4j2
@@ -84,6 +72,5 @@ public class Config {
 
         assert credential != null;
         return new GraphServiceClient(new AzureIdentityAuthenticationProvider(credential,new String[0], scopes), okHttpClient);
-
     }
 }
